419ebf064aa5c22c09a90e4e954f5d802e28e297
KdanielaP <135910567+KdanielaP@users.noreply.github.com>
2024-03-02 10:31:05 -0600

    first commit

---
+++ Parcial2/Parcial2/proyecto_2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2
+{
+    internal class proyecto_2
+    {
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese la cantidad de numeros a ingresar: ");
+        int cantidadNumeros = Convert.ToInt32(Console.ReadLine());
+
+        int[] numeros = new int[cantidadNumeros];
+
+        for (int i = 0; i < cantidadNumeros; i++)
+        {
+            Console.Write("Ingrese el numero " + (i + 1) + ": ");
+            numeros[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int cantidadPares = 0, cantidadImpares = 0;
+        int sumaPares = 0, sumaImpares = 0;
+
+        for (int i = 0; i < cantidadNumeros; i++)
+        {
+            if (numeros[i] % 2 == 0)
+            {
+                cantidadPares++;
+                sumaPares += numeros[i];
+            }
+            else
+            {
+                cantidadImpares++;
+                sumaImpares += numeros[i];
+            }
+        }
+
+        Console.WriteLine("Cantidad de numeros pares: " + cantidadPares);
+        Console.WriteLine("Cantidad de numeros impares: " + cantidadImpares);
+        Console.WriteLine("Suma de numeros pares: " + sumaPares);
+        Console.WriteLine("Suma de numeros impares: " + sumaImpares);
+    }
+}
---
+++ parcial/parcial/Calculadora.cs
@@ -0,0 +1,92 @@
+﻿namespace Parcial
+{
+    using System;
+
+    public class Calculadora
+    {
+        public int Sumar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Restar(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Multiplicar(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Dividir(int num1, int num2)
+        {
+            if (num2 == 0)
+            {
+                Console.WriteLine("Error: No se puede dividir entre cero.");
+                return 0;
+            }
+            return num1 / num2;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Crear una instancia de la clase Calculadora
+            Calculadora miCalculadora = new Calculadora();
+
+            // Ejecutar los cuatro métodos de operaciones básicas
+            int resultadoSuma = miCalculadora.Sumar(10, 5);
+            Console.WriteLine("Suma: " + resultadoSuma);
+
+            int resultadoResta = miCalculadora.Restar(10, 5);
+            Console.WriteLine("Resta: " + resultadoResta);
+
+            int resultadoMultiplicacion = miCalculadora.Multiplicar(10, 5);
+            Console.WriteLine("Multiplicación: " + resultadoMultiplicacion);
+
+            int resultadoDivision = miCalculadora.Dividir(10, 5);
+            Console.WriteLine("División: " + resultadoDivision);
+
+            // Llamar al método estático para sumar dos valores
+            SumarDosValores();
+        }
+
+        public static void SumarDosValores()
+        {
+            try
+            {
+                // Pedir dos valores de entrada
+                Console.WriteLine("Ingrese el primer valor:");
+                string valor1Str = Console.ReadLine();
+
+                Console.WriteLine("Ingrese el segundo valor:");
+                string valor2Str = Console.ReadLine();
+
+                // Convertir los valores a enteros
+                int valor1 = int.Parse(valor1Str);
+                int valor2 = int.Parse(valor2Str);
+
+                // Sumar los valores
+                int suma = valor1 + valor2;
+
+                // Mostrar el resultado
+                Console.WriteLine($"La suma de {valor1} y {valor2} es: {suma}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Debe ingresar valores numéricos.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Fin del programa.");
+            }
+        }
+    }
+}
